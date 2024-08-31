e4a3e6e940d484e7a7d3b56ada117aca43e3a293
denisc911 <denis.cabrera1990@gmail.com>
2024-08-31 08:40:06 +0200

    Modificado el APP.jsv para poner productos ademas agregados los context de products

---
+++ src/context/ProductsContext/ProductsState.js
@@ -0,0 +1,29 @@
+import { createContext, useReducer } from 'react'
+import axios from 'axios'
+import ProductsReducer from './ProductsReducer'
+
+const initialState = {
+ products: [],
+}
+
+const API_URL = 'http://localhost:3001'
+
+export const ProductsProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(ProductsReducer, initialState)
+
+const getProducts = async () => {
+}
+
+return (
+    <ProductsContext.Provider
+      value={{
+        products: state.products,
+        getProducts,
+      }}
+    >
+      {children}
+    </ProductsContext.Provider>
+  )
+}    
+
+export const ProductsContext = createContext(initialState)
