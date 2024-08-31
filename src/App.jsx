7afe052390314868be96b57963bbeead9e606a7d
denisc911 <denis.cabrera1990@gmail.com>
2024-08-31 12:23:56 +0200

    cambios aplicados app y productsreducer

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
          <Routes>
            <Route path="/" element={<Login />} />
            <Route path="/profile" element={<Profile />} />
-           <Route path="/register" element={<Register />} />
+           {/* <Route path="/register" element={<Register />} /> */}
            <Route path="/products" element={<Products />} />
          </Routes>
        </Router>
