eed9b616cd73ab58f7bb1aa30a4fa21cc8add29f
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-09-05 11:53:21 +0200

    Finished correcting the problems that arose with the admin component that caused errors when creating and editing products. Everything works correctly. Refactored the use of API_URL through dotenv, created .env file to facilitate the process.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,25 +1,28 @@
 import { useContext, useEffect } from 'react'
 import { ProductsContext } from '../../context/ProductsContext/ProductsState'
-const Products = () => {
-  const { getProducts, products, addCart, cart } = useContext(ProductsContext)
- useEffect(() => {
-   getProducts()
- }, [])
- 
- useEffect(() => {
-  localStorage.setItem('cart', JSON.stringify(cart))
-}, [cart])
 
- return (
-   <>
-     {products && products.map((product) => (
-         <div key={product.id}>
-           <span>{product.productName} </span>
-           <span>{product.price.toFixed(2)}</span>
-           <button onClick={() => addCart(product)}>Add Cart</button>
-         </div>
-       ))}
-   </>
- )
+const Products = () => {
+    const { getProducts, products, addCart, cart } = useContext(ProductsContext)
+
+    useEffect(() => {
+        getProducts()
+    }, [getProducts])
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
+
+    return (
+        <>
+            {products && products.map((product) => (
+                <div key={product.id}>
+                    <span>{product.productName} </span>
+                    <span>{product.price !== undefined ? product.price.toFixed(2) : 'N/A'}</span>
+                    <button onClick={() => addCart(product)}>Add Cart</button>
+                </div>
+            ))}
+        </>
+    )
 }
+
 export default Products
