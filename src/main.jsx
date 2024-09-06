74026c32b7152a4b67294bb10b9683cf4e3bc518
denisc911 <denis.cabrera1990@gmail.com>
2024-09-06 19:43:58 +0200

    agregado el reset de css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
+import './reset.css'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
